bca2215696af2647cf8e43d64508836b7de1d296
Ricardo Nario <diguibal@gmail.com>
2025-04-08 13:17:39 -0300

    Descripción del cambio, por ejemplo: Configurar script start para Render

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Permite que el servidor sea accesible externamente
+    port: 3000, // Puerto que Render puede detectar
+  },
+  preview: {
+    host: "0.0.0.0", // Configuración para "vite preview"
+    port: 3000, // Puerto que Render puede detectar
+  },
+});
